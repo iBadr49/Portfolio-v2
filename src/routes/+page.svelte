47b4a7c05c8dc2df4ffa33d584ffff0e8102dc5b
iBadr49 <badr.amara@hva.nl>
2024-05-19 16:03:46 +0200

    Calling components to the page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,15 @@
+<script>
+  import Header from "$lib/components/organisms/header.svelte";
+  import Introduction from "$lib/components/molecules/introduction.svelte";
+  import About from "$lib/components/molecules/about.svelte";
+  import Skillz from "$lib/components/molecules/skillz.svelte";
 
-<h1>
-  Badr Amara
-</h1>
+</script>
+
+<Header />
+<Introduction />
+
+<About />
+
+<Skillz />
+
