9b1c9a0cfe9e48bb23bfdb9594c98bb47b35e6aa
iBadr49 <badr.amara@hva.nl>
2024-06-04 14:54:26 +0200

    About styling changes

--- src/lib/components/molecules/about.svelte
+++ src/lib/components/molecules/about.svelte
@@ -1,6 +1,6 @@
 <section class="about-section">
   <div class="image-content">
-    <img src="images/1.svg" alt="" />
+    <img src="images/1.svg" alt="my_apple_moji" />
   </div>
   <div class="text-content">
     <h2>About</h2>
@@ -12,10 +12,14 @@
 
     <p>
       I started making websites in 2021, and in 2022, I pursued a course where I
-      could further develop my passion for web design & developement . Since
-      then, I have been able to build various websites for myself and for
-      others.
+      could further develop my passion for web design & development. Since then,
+      I have been able to build various websites for myself and for others.
     </p>
+
+    <div class="button-group">
+      <a href="/" class="work-button">See my work</a>
+      <a href="/" class="contact-button">Contact Me</a>
+    </div>
   </div>
 </section>
 
@@ -23,7 +27,6 @@
   .about-section {
     display: flex;
     justify-content: space-around;
-    /* justify-content: center; */
     align-items: center;
     padding: 20px;
   }
@@ -37,16 +40,68 @@
     max-width: 25rem;
   }
 
+  .text-content {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
   .text-content h2 {
     text-decoration: underline var(--Tea-green);
     font-size: 3rem;
+    color: var(--Boiling-magma);
   }
-
-
 
   .text-content p {
     max-width: 600px;
     margin-top: 1em;
-    font-size: 20px;
+  }
+
+  .button-group {
+    margin-top: 1.5em;
+    display: flex;
+    gap: 1em;
+  }
+
+  .work-button,
+  .contact-button {
+    text-decoration: none;
+    padding: 10px 10px;
+    color: black;
+    text-transform: uppercase;
+    font-size: larger;
+    padding: 0.5rem 0.75rem;
+    border: 1px solid var(--Tea-green);
+    border-radius: 0.5rem;
+    box-shadow: -2px 2px 0 0 var(--Tea-green);
+    transition: 0.4s ease-out;
+    cursor: pointer;
+    &:disabled {
+      cursor: not-allowed;
+    }
+    &:hover {
+      transform: translate(-1px 1px);
+      box-shadow: -1px 1px 0 0 var(--Tea-green);
+      /* color: var(--Tea-green); */
+    }
+  }
+
+
+
+  /* Media Queries */
+  @media (max-width: 768px) {
+    .about-section {
+      flex-direction: column;
+      align-items: flex-start;
+    }
+
+    .image-content {
+      display: none;
+    }
+
+    .text-content {
+      align-items: flex-start;
+    }
   }
 </style>
+
