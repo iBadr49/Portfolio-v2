a78b4b25eb8801f9f6e6d38034d92b326b63d9c8
iBadr49 <badr.amara@hva.nl>
2024-06-04 14:53:54 +0200

    Add contact
    Added contact -text component

---
+++ src/lib/components/molecules/contact.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<p>
+  Have an idea or a project you need my help with? Or just have a question about
+  my work? Don’t hesitate to get in touch.
+
+  <br> <br>
+
+  You can also follow me on [LinkedIn/Github] for updates on my work.
+</p>
+
+<style>
+  p {
+    max-width: 35rem;
+    margin: 1rem 0;
+  }
+</style>
