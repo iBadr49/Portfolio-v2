162a50c5a0e45f887951fe590b3592b083e93dc2
iBadr49 <badr.amara@hva.nl>
2024-06-11 23:53:13 +0200

    Added socials link

--- src/lib/components/molecules/contact.svelte
+++ src/lib/components/molecules/contact.svelte
@@ -5,14 +5,42 @@
   Have an idea or a project you need my help with? Or just have a question about
   my work? Don’t hesitate to get in touch.
 
-  <br> <br>
+  <br /> <br />
 
-  You can also follow me on [LinkedIn/Github] for updates on my work.
+  You can also follow me on <a href="/"> Linkedin</a> and <a href="/"> Github </a> for updates on my work.
 </p>
+
+
 
 <style>
   p {
     max-width: 35rem;
     margin: 1rem 0;
   }
+
+a {
+  color: var(--Boiling-magma);
+  position: relative;
+  text-decoration: none;
+  outline: none;
+}
+
+a::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  height: 0.17em;
+  background-color: var(--Tea-green);
+  scale: 0 1;
+  transform-origin: right center;
+  transition: scale 0.3s;
+}
+
+a:hover:after,
+a:focus-visible:after {
+  scale: 1 1;
+  transform-origin: left center;
+}
 </style>
